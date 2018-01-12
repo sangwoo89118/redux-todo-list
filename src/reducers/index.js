4583c52370ef586ce89b664e807e2468c5559844
sangwoo89118 <sangwoo89118@gmail.com>
2018-01-11 17:03:22 -0800

    Completed add item with dummy data

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 
 
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import todoReducer from './todo_reducer';
 
 export default combineReducers({
-  todo: todoReducer
+  todo: todoReducer,
+  form: formReducer
 })
